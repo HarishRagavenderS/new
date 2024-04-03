eae4451437834cb41659b53e4eafa99abfa8cad3
HarishRagavenderS <140372051+HarishRagavenderS@users.noreply.github.com>
2024-04-03 19:41:16 +0530

    new2

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       double  hello = 23.845d;
+       int init = (int) hello;
+       System.out.println(hello);
+       System.out.println(init);
+    };
+}
